aba4af0cc70d2b24a5aad2cebc4de5aae69f0624
Deluan <deluan@thoughtworks.com>
2014-11-18 00:14:15 -0600

    Adding Spock

---
+++ src/test/groovy/com/deluan/isonic/SampleSpec.groovy
@@ -0,0 +1,11 @@
+package com.deluan.isonic
+
+import spock.lang.Specification
+
+class SampleSpec extends Specification {
+
+    def 'this is a spec'() {
+        expect:
+          1 + 2 == 3
+    }
+}
