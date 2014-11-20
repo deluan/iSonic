00f83d4afec047cfc9449f81132e3c20aa099161
Deluan <deluan@thoughtworks.com>
2014-11-20 00:41:51 -0600

    Introduced Solr

--- src/main/groovy/com/deluan/isonic/Application.groovy
+++ src/main/groovy/com/deluan/isonic/Application.groovy
@@ -1,14 +1,25 @@
 package com.deluan.isonic
 
+import org.apache.solr.client.solrj.SolrServer
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer
+import org.apache.solr.core.CoreContainer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.solr.core.SolrTemplate
+import org.springframework.data.solr.repository.config.EnableSolrRepositories
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableSolrRepositories("com.deluan.isonic.repository")
 public class Application {
+
+    private Logger logger = LoggerFactory.getLogger(this.class)
 
     static void main(String[] args) {
         def ctx = SpringApplication.run(Application.class, args)
@@ -16,8 +27,25 @@ public class Application {
         println("Let's inspect the beans provided by Spring Boot:")
 
         ctx.beanDefinitionNames.sort().each { beanName ->
-            println("   ${beanName}");
+            println("   ${beanName}")
         }
+    }
+
+    @Bean
+    public SolrServer solrServer() {
+        File solrHome = new File("solr")
+        System.setProperty("solr.solr.home", solrHome.absolutePath)
+        System.setProperty("solr.data.dir", new File("tmp/solr/data").absolutePath)
+
+        def container = new CoreContainer(solrHome.absolutePath)
+        container.load()
+
+        new EmbeddedSolrServer(container, "collection1")
+    }
+
+    @Bean
+    public SolrTemplate solrTemplate(SolrServer server) throws Exception {
+        return new SolrTemplate(server)
     }
 
 }
