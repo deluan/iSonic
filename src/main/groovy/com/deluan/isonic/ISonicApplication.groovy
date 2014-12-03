b39335715e4b9b2fddcb622f120b2dec13b0c7cd
Deluan <github@deluan.com>
2014-12-03 01:34:11 -0500

    Using new @SpringBootApplication annotation

--- src/main/groovy/com/deluan/isonic/ISonicApplication.groovy
+++ src/main/groovy/com/deluan/isonic/ISonicApplication.groovy
@@ -1,11 +1,9 @@
 package com.deluan.isonic
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class ISonicApplication {
 
     static void main(String[] args) {
